6b492c503db9b3dc2706a627483e0f331724e147
Jolvary98 <32238907+jolvary@users.noreply.github.com>
2019-05-16 00:29:53 +0200

    Commit Nº7
    Vídeo 8

--- src/boletin/admin.py
+++ src/boletin/admin.py
@@ -3,4 +3,13 @@ from .models import Registrado
 
 # Register your models here.
 
-admin.site.register(Registrado)
+class AdminRegistrado(admin.ModelAdmin):
+	list_display  =  ["email", "nombre", "timestamp"]
+	#list_display_list = ["email"]
+	list_filter = ["timestamp"]
+	list_editable = ["nombre"]
+	search_fields = ["email", "nombre"]
+	class Meta:
+		moderl = Registrado
+
+admin.site.register(Registrado, AdminRegistrado)
